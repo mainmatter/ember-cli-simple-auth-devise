a70a3edd4aca55282237474ad4639d4d30b6a602
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-03-15 21:11:40 +0100

    simplified included hook

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ module.exports = {
   name: 'Ember CLI Simple Auth Devise',
 
   included: function(app) {
-    this._super.included(app);
-
-    this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-devise.amd.js', {
+    app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-devise.amd.js', {
       exports: {
         'simple-auth-devise/authenticators/devise': ['default'],
         'simple-auth-devise/authorizers/devise':    ['default'],
